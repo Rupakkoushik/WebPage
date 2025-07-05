70e10449c043297aaf3f628ba72db193d1e9ae4c
Rupak Koushik <147837885+Rupakkoushik@users.noreply.github.com>
2025-07-05 19:26:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function openForm(formType) {
+  if (formType === 'fullTimeForm') {
+    
+    window.open('https://docs.google.com/forms/d/1qRuADm2meRawnze3Zfeo6JF1T7y7Y2UbVyvqze-68vU/edit', '_blank');
+  } else if (formType === 'internshipForm') {
+
+    window.open('https://docs.google.com/forms/d/1QfckCysrXS-ORzWuBa5pVcCwClHQUYFRUnng50J0GtY/edit', '_blank');
+  }
+}
